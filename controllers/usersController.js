98cefe762d2eded3f7617ce26dc77463d3c830a7
DenMerch <rosignolli@gmail.com>
2023-06-18 20:02:26 +0300

    add upload

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,22 +1,28 @@
 const bcrypt = require("bcryptjs")
 const jwt = require("jsonwebtoken")
+const gravatar = require('gravatar');
+const path = require('node:path');
+const fs = require("fs/promises");
 
 const User = require("../models/user")
 const { createNewError } = require("../helpers")
 const { cntrWrap } = require('../decorator');
 const { SECRET_KEY } = process.env;
+const avatrsDir = path.join(process.cwd(), "public", "avatars");
+
 
 const registerUser = async (req, res) => {
     const { email, password } = req.body;
 
     const user = await User.findOne({ email })
+    const avatar = gravatar.url(email);
 
     if (user) {
         throw createNewError(409, "Email in use")
     }
     const cryptPassw = await bcrypt.hash(password, 10)
 
-    const newUser = await User.create({ ...req.body, password: cryptPassw });
+    const newUser = await User.create({ ...req.body, password: cryptPassw, avatarURL: avatar });
 
     res.status(201).json({
         user: {
@@ -77,12 +83,29 @@ const updateSubscription = async (req, res) => {
     })
 }
 
+const updateAvatar = async (req, res) => {
+    const { id } = req.user
+    const { path, filename } = req.file
+    console.log(avatrsDir);
+    console.log(filename);
+    let createdNewPath = path.join('/foo', 'bar', 'baz/asdf', 'quux')
+    console.log(id, createdNewPath);
+
+    // console.log(newPath);
+    // await fs.rename(path, newPath)
+    // const avatar = path.join("avatars", filename)
+    // const result = await User.findByIdAndUpdate(id, { avatarURL: avatar })
+    res.status(200).json({ message: "done" })
+
+}
+
 
 module.exports = {
     registerUser: cntrWrap(registerUser),
     loginUser: cntrWrap(loginUser),
     getCurrent: cntrWrap(getCurrent),
     logoutUser: cntrWrap(logoutUser),
-    updateSubscription: cntrWrap(updateSubscription)
+    updateSubscription: cntrWrap(updateSubscription),
+    updateAvatar: cntrWrap(updateAvatar)
 
 }
--- middlwares/index.js
+++ middlwares/index.js
@@ -1,9 +1,10 @@
 const isValidId = require("./isValidId")
 const handleMongooseError = require("./handleMongooseError")
+const upload = require("./upload")
 
 
 module.exports = {
     isValidId,
     handleMongooseError,
-
+    upload,
 }
